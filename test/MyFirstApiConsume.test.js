969ff9f62a387f6818864d8681bd212aad3857c9
PSLCOL\lzapata <lzapata@psl.com.co>
2018-02-24 21:17:01 -0500

    4. Verificación de Código Estático

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,71 +1,66 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
-const chai = require('chai');
-
-const expect = chai.expect;
+const { expect } = require('chai').expect;
 
 describe('First Api Tests', () => {
+  it('Consume GET Service', () => agent.get('https://httpbin.org/ip').then((response) => {
+    expect(response.status).to.equal(statusCode.OK);
+    expect(response.body).to.have.property('origin');
+  }));
 
-    it('Consume GET Service', () => {
-        return agent.get('https://httpbin.org/ip').then((response) => {
-          expect(response.status).to.equal(statusCode.OK);
-          expect(response.body).to.have.property('origin');
-        });
+  it('Consume GET Service with query parameters', () => {
+    const query = {
+      name: 'John',
+      age: '31',
+      city: 'New York'
+    };
+
+    return agent.get('https://httpbin.org/get')
+      .query(query)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(query);
       });
+  });
 
-      it('Consume GET Service with query parameters', () => {
-        const query = {
-          name: 'John',
-          age: '31',
-          city: 'New York'
-        };
-      
-        return agent.get('https://httpbin.org/get')
-          .query(query)
-          .then((response) => {
-            expect(response.status).to.equal(statusCode.OK);
-            expect(response.body.args).to.eql(query);
-          });
+  it('Consume HEAD Service', () => {
+    agent.head('https://httpbin.org/headers')
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body).to.eql({});
       });
+  });
 
-      it('Consume HEAD Service', () => {
-        agent.head('https://httpbin.org/headers')
-          .then((response) => {
-            expect(response.status).to.equal(statusCode.OK);
-            expect(response.body).to.eql({});
-          });
+  it('Consume PATCH Service', () => {
+    const body = { name: 'Linda', lastname: 'Zapata' };
+
+    return agent.patch('https://httpbin.org/patch')
+      .send(body)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.json).to.eql(body);
       });
+  });
 
-      it('Consume PATCH Service', () => {
-        const body = {name: 'Linda', lastname: 'Zapata'};
+  it('Consume PUT Service', () => {
+    const body = { name: 'Linda', lastname: 'Zapata' };
 
-        return agent.patch('https://httpbin.org/patch')
-          .send(body)
-          .then((response) => {
-            expect(response.status).to.equal(statusCode.OK);
-            expect(response.body.json).to.eql(body);
-          });
+    return agent.put('https://httpbin.org/put')
+      .send(body)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.json).to.eql(body);
       });
+  });
 
-      it('Consume PUT Service', () => {
-        const body = {name: 'Linda', lastname: 'Zapata'};
+  it('Consume DELETE Service', () => {
+    const body = { name: 'Linda', lastname: 'Zapata' };
 
-        return agent.put('https://httpbin.org/put')
-          .send(body)
-          .then((response) => {
-            expect(response.status).to.equal(statusCode.OK);
-            expect(response.body.json).to.eql(body);
-          });
+    return agent.del('https://httpbin.org/delete')
+      .send(body)
+      .then((response) => {
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.json).to.eql(body);
       });
-
-      it('Consume DELETE Service', () => {
-        const body = {name: 'Linda', lastname: 'Zapata'};
-
-        return agent.del('https://httpbin.org/delete')
-          .send(body)
-          .then((response) => {
-            expect(response.status).to.equal(statusCode.OK);
-            expect(response.body.json).to.eql(body);
-          });
-      });
+  });
 });
