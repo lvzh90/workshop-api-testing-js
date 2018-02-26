ddb12ddad5cdde7f69d72dd11a5058d52b8e6496
PSLCOL\lzapata <lzapata@psl.com.co>
2018-02-26 07:47:55 -0500

    4. Verificación de Código Estático

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,6 +1,6 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
 const statusCode = require('http-status-codes');
-const { expect } = require('chai').expect;
+const { expect } = require('chai');
 
 describe('First Api Tests', () => {
   it('Consume GET Service', () => agent.get('https://httpbin.org/ip').then((response) => {
