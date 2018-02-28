2b4d163bec20a668f2cf2435ce6c91373b166e61
PSLCOL\lzapata <lzapata@psl.com.co>
2018-02-28 12:50:53 -0500

    6. Consumiendo Métodos GET

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -11,45 +11,66 @@ describe('Github Repositories Test', () => {
     const urlBase = 'https://api.github.com';
     const githubUserName = 'aperdomob';
     let userSession;
+    let statusCodeRequest;
 
-    it('Validate userName, company and location', () => agent.get(`${urlBase}/users/${githubUserName}`)
-      .then((response) => {
-        userSession = response.body;
+    before((done) => {
+      agent.get(`${urlBase}/users/${githubUserName}`)
+        .then((response) => {
+          userSession = response.body;
+          statusCodeRequest = response.status;
+          done();
+        });
+    });
 
-        expect(response.status).to.equal(statusCode.OK);
-        expect(userSession.login).equal(githubUserName);
-        expect(userSession.name).equal('Alejandro Perdomo');
-        expect(userSession.company).equal('PSL');
-        expect(userSession.location).equal('Colombia');
-      }));
+    it('Validate userName, company and location', () => {
+      expect(statusCodeRequest).to.equal(statusCode.OK);
+      expect(userSession.login).equal(githubUserName);
+      expect(userSession.name).equal('Alejandro Perdomo');
+      expect(userSession.company).equal('PSL');
+      expect(userSession.location).equal('Colombia');
+    });
 
-    describe('• Get repository list', () => {
+    describe('Get repository list', () => {
       const expectedRepository = 'jasmine-awesome-report';
       let repositories;
       let repository;
 
-      it(`${expectedRepository} repository`, () => agent.get(userSession.repos_url)
-        .then((response) => {
-          repositories = response.body;
-          repository = repositories.find(repo => repo.name === expectedRepository);
-
-          assert.exists(repository);
-          expect(repository.full_name).to.equal('aperdomob/jasmine-awesome-report');
-          expect(repository.private).to.equal(false);
-          expect(repository.description).to.equal('An awesome html report for Jasmine');
-        }));
-
-      describe(`• Download ${expectedRepository} repository`, () => {
-        it('Downloading...', () => agent.get(`${repository.html_url}/archive/${repository.default_branch}.zip`)
-          .buffer(true)
+      before((done) => {
+        agent.get(userSession.repos_url)
           .then((response) => {
-            const zip = response.text;
-            expect(response.status).to.equal(statusCode.OK);
-            assert.isNotNull(md5(zip));
-          }));
+            repositories = response.body;
+            repository = repositories.find(repo => repo.name === expectedRepository);
+            done();
+          });
+      });
 
-        describe('• Search the list of files in the repository, the file README.md', () => {
-          let files;
+      it(`${expectedRepository} repository`, () => {
+        assert.exists(repository);
+        expect(repository.full_name).to.equal('aperdomob/jasmine-awesome-report');
+        expect(repository.private).to.equal(false);
+        expect(repository.description).to.equal('An awesome html report for Jasmine');
+      });
+
+      describe(`Download ${expectedRepository} repository`, () => {
+        let zip;
+        let statusCodeRepositoryRequest;
+
+        before((done) => {
+          agent.get(`${repository.html_url}/archive/${repository.default_branch}.zip`)
+            .buffer(true)
+            .then((response) => {
+              zip = response.text;
+              statusCodeRepositoryRequest = response.status;
+              done();
+            });
+        });
+
+        it('Downloading...', () => {
+          expect(statusCodeRepositoryRequest).to.equal(statusCode.OK);
+          assert.isNotNull(md5(zip));
+        });
+
+        describe('Search the list of files in the repository, the file README.md', () => {
           let readmeFile;
 
           const format = {
@@ -58,21 +79,34 @@ describe('Github Repositories Test', () => {
             sha: '9bcf2527fd5cd12ce18e457581319a349f9a56f3'
           };
 
-          it('Searching...', () => agent.get(`${repository.url}/contents`)
-            .then((response) => {
-              files = response.body;
-              readmeFile = files.find(file => file.name === 'README.md');
-
-              assert.exists(readmeFile);
-              expect(readmeFile).containSubset(format);
-            }));
-
-          describe('• Download README.md', () => {
-            it('Downloading...', () => agent.get(readmeFile.download_url)
+          before((done) => {
+            agent.get(`${repository.url}/contents`)
               .then((response) => {
-                const contentFile = response.text;
-                assert.isNotNull(md5(contentFile));
-              }));
+                const files = response.body;
+                readmeFile = files.find(file => file.name === 'README.md');
+                done();
+              });
+          });
+
+          it('Searching...', () => {
+            assert.exists(readmeFile);
+            expect(readmeFile).containSubset(format);
+          });
+
+          describe('Download README.md', () => {
+            let contentFile;
+
+            before((done) => {
+              agent.get(readmeFile.download_url)
+                .then((response) => {
+                  contentFile = response.text;
+                  done();
+                });
+            });
+
+            it('Downloading...', () => {
+              assert.isNotNull(md5(contentFile));
+            });
           });
         });
       });
